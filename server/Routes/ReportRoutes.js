abdc6c133f0d3921adb439122e476b5406dedd4c
Ashraful momin <ashrafulmomin7076@gmail.com>
2025-10-01 05:39:51 +0530

    vercer.json added with Rate limiter Added

--- server/Routes/ReportRoutes.js
+++ server/Routes/ReportRoutes.js
@@ -1,8 +1,23 @@
 import express from "express";
 import openAi_Api from "../Services/OpenAiApi.js";
+import rateLimit from "express-rate-limit";
 
 
 const router = express.Router();
+
+
+
+router.use(rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, 
+  max: 1, 
+  message: {
+    success: false,
+    error: "Daily limit reached",
+    info: "You can only make 1 free request per 24 hours from the same IP.",
+    support: "If you'd like unlimited access and to help us cover hosting & API costs, please consider donating."
+  }
+}));
+
 
 router.post("/create-report" , async(req , res) => {
   const { text } = req.body;
